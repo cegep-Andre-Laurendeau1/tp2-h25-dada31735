210d617c84ecd95360d5807e2e5ca25565ed640d
Dumitru Gliga <anagliga31@gmail.com>
2025-03-08 15:22:26 -0500

    ajout des annotations hybernate a Livre

--- src/main/java/ca/cal/tp2/modele/Livre.java
+++ src/main/java/ca/cal/tp2/modele/Livre.java
@@ -1,6 +1,14 @@
 package ca.cal.tp2.modele;
 
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@DiscriminatorValue("L")
 public class Livre extends Document {
     private String ISBN;
     private String auteur;
