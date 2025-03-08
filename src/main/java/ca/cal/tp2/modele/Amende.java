11a35ed96fef0bd849134e459f4a07342c308c3d
Dumitru Gliga <anagliga31@gmail.com>
2025-03-08 15:09:35 -0500

    ajout des annotations Hibernate a la classe Amende

--- src/main/java/ca/cal/tp2/modele/Amende.java
+++ src/main/java/ca/cal/tp2/modele/Amende.java
@@ -1,14 +1,27 @@
 package ca.cal.tp2.modele;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.LocalDate;
 import java.util.Date;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 class Amende {
-    private int fineID;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long fineID;
     private double montant;
-    private Date dateCreation;
+    private LocalDate dateCreation;
     private boolean status;
 
     public void calculMontant() {}
