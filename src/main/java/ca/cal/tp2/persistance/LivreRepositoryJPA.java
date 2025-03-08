16f9cf29c06947a78357f204009a658f22b0c5ad
Dumitru Gliga <anagliga31@gmail.com>
2025-03-08 18:55:10 -0500

    creation du LivreRepository avec JPA

---
+++ src/main/java/ca/cal/tp2/persistance/LivreRepositoryJPA.java
@@ -0,0 +1,33 @@
+package ca.cal.tp2.persistance;
+
+import ca.cal.tp2.modele.Livre;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class LivreRepositoryJPA implements LivreRepository {
+    private final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("tp2");
+
+    @Override
+    public void save(Livre livre) {
+        try (EntityManager em = entityManagerFactory.createEntityManager()) {
+            em.getTransaction().begin();
+            em.persist(livre);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Livre get(long id) {
+        try(EntityManager em = entityManagerFactory.createEntityManager()) {
+            return em.createQuery("SELECT l FROM Livre l WHERE l.documentID = :id", Livre.class)
+                    .setParameter("id", id)
+                    .getSingleResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
